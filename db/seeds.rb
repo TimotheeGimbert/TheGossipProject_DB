f0a32006da1e5af14a3cd6648465ab713c726c9e
TimotheeGimbert <timothee.gimbert@free.fr>
2021-10-28 22:37:45 +0200

    adds users and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  user = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    description: Faker::Lorem.sentence(word_count: 40, supplemental: true),
+    email: Faker::Internet.email,
+    age: rand(18..120)
+    )
+    puts user
+end
